98af9e5c7c5839c6e28444940520e4c82f4eb6c5
Joshua Verbiano Inkiriwang <63942453+JoshInkiriwang@users.noreply.github.com>
2021-04-29 09:21:55 +0700

    Add files via upload

---
+++ R Function.R
@@ -0,0 +1,49 @@
+ls()
+log(8)
+# a = 1
+log(a)
+
+#Nested Function
+exp(1)
+log(exp(1))
+
+#Help Files
+help("log")
+?log
+
+#Function with Argument
+args(log)
+log(8,base=2)
+log(x=8,base=2)
+log(8,2)
+
+2^3
+
+help("+")
+?"+"
+
+#Untuk Melihat available dataset
+data()
+#Access Dataset
+CO2
+
+#Mathematical Object Pre Built
+pi
+Inf
+
+#Variable Names
+#Must start with a letter 
+#And can't contain spaces
+#Don't use already defined names in R
+#example
+# Put number in variable
+a <- 3
+b <- -5
+c <- 2
+
+solution_1 <- (-b + sqrt(b^2 - 4*a*c) ) / (2*a)
+solution_2 <- (-b - sqrt(b^2 - 4*a*c) ) / (2*a)
+
+#Output print
+print(solution_1)
+print(solution_2)
